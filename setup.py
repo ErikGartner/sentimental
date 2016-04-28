e5223284b3b8b6a644cb450da4f76594c1ff4097
Erik Gärtner <erik@gartner.io>
2016-04-28 14:02:38 +0200

    fix: Add package data

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ A sentiment analyzer
         'Operating System :: OS Independent',
     ],
     packages=find_packages(),
-    include_package_data=False,
+    include_package_data=True,
     install_requires=[
         'numpy>=1.11.0',
         'scipy>=0.17.0',
